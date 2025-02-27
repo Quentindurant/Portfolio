ac6acb2e3789a44309b80005bd139cc96115b5b2
QuentinDurant <quentin.durant49@orange.fr>
2025-02-27 10:45:52 +0100

    Update header.php

--- dark/partials/header.php
+++ dark/partials/header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>Protefolio</title>
+    <title>portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Template CSS Files -->

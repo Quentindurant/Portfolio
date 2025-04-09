a2d7a089b0c6162d7d27c2541365aabf3bdaf8ef
QuentinDurant <quentin.durant49@orange.fr>
2025-04-09 11:52:23 +0200

    Update exercice.php

--- dark/exercice.php
+++ dark/exercice.php
@@ -47,9 +47,9 @@
             width: 100%;
             height: 100%;
             display: flex;
-            align-items: center;
+            align-items: flex-start;
             justify-content: center;
-            padding: 50px 0;
+            padding-top: 100px;
         }
         
         .portfolio .slideshow figure {

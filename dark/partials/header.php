c140ef83d31adc26afe830797b60530dc33c9f68
QuentinDurant <quentin.durant49@orange.fr>
2025-04-24 15:07:13 +0200

    Update header.php

--- dark/partials/header.php
+++ dark/partials/header.php
@@ -2,7 +2,6 @@
     <meta charset="utf-8">
     <title>portfolio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" sizes="32x32" href="image/FaviconQD.png">
     <link rel="shortcut icon" href="image/FaviconQD.png">
 
     <!-- Template CSS Files -->
